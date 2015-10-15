57fbd97b82a1a381439984de2f0b09b06a0a7610
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2015-10-14 20:43:31 -0500

    style shared

--- communications/shared.c
+++ communications/shared.c
@@ -5,34 +5,45 @@
 #include <stdlib.h>
 
 int main() {
-	int a=16,id,r,*ap;
-	key_t key=321;
+	int a = 16, id, r, *ap;
+	key_t key = 321;
 	pid_t pid;
 
-	id=shmget(key,sizeof(int),IPC_CREAT|0666);
-	//validar error
-	if(id==(-1)){
-		perror("no pudeasereje");
-		exit (-1);
-			}
-	ap=shmat(id,(void*)NULL,0);
-	//validar error
-	if(*ap==(-1)){
-		perror("no pude");
-		exit (-1);
-			}
+	// creates shared memory identifier
+	id = shmget(key, sizeof(int), IPC_CREAT|0666);
+	if (id == -1) {
+		perror("Unable to create memory space");
+		exit(-1);
+	}
+
+	// maps the shared memory segment
+	ap = shmat(id, (void*)NULL, 0);
+	if (*ap == -1) {
+		perror("Unable to map memory space");
+		exit(-1);
+	}
 	
-	
-	pid=fork();
-	
-	//validar//
-	if (pid==0)
-		{
-			printf("%i \n",*ap);
-		}
-		else
-		{
-		*ap=16;
-		}
-	shmdt(ap);
+	// creates a child
+	pid = fork();
+	if (pid == -1) {
+		perror("Unable to create child");
+		exit(-1);
+	}
+
+	// child
+	if (pid == 0) {
+		printf("%i \n", *ap);
+	}
+
+	// parent
+	else {
+		*ap = 16;
+	}
+
+	// unmaps the shared memory segment
+	r = shmdt(ap);
+	if (r == -1) {
+		perror("Unable to unmap memory space");
+		exit(-1);
+	}
 }
