b977d5e735290e3bb0cca5d584774d33fe91b3ba
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2015-10-19 07:08:18 -0500

    feat(segunda): basic communication system

--- practicas/segunda/p2-dogClient.c
+++ practicas/segunda/p2-dogClient.c
@@ -6,34 +6,147 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #define PORT 3535
 
-int main(int argc, char *argv[])
-{
+/**
+ * Imprime el menu
+ */
+void menu() {
+	printf("\n");
+	printf("MENU: \n");
+	printf("[i]: Ingresar registro\n");
+	printf("[v]: Ver registro\n");
+	printf("[o]: Borrar registro\n");
+	printf("[u]: Buscar registro\n");
+	printf("[s]: Salir\n");
+	fflush(stdout);
+}
+
+/**
+ * Cast string a minuscula
+ */
+void lowstr(char *s) {
+	char  *p;
+	for (p = s; *p != '\0'; p++)
+		*p = (char) tolower(*p);
+}
+
+void create() {}
+
+void list() {}
+
+void erase() {}
+
+void search() {}
+
+/**
+ * Funcion principal
+ */
+int main(int argc, char *argv[]) {
     int clientfd, r;
     struct sockaddr_in client;
     struct hostent *he;
     char buffer[32];
 
+    // abre el socket
     clientfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(clientfd < 0){
+    if(clientfd < 0) {
         perror("\n-->Error en socket():");
         exit(-1);
     }
     client.sin_family = AF_INET;
     client.sin_port = htons(PORT);
-
     inet_aton(argv[1], &client.sin_addr);
 
+    // conecta al servidor
     r = connect(clientfd, (struct sockaddr *)&client, (socklen_t)sizeof(struct sockaddr));
-    if(r < 0){
+    if(r < 0) {
         perror("\n-->Error en connect(): ");
         exit(-1);
     }
+
+    // espera coneccion
     r = recv(clientfd, buffer, 32, 0);
     buffer[r] = 0;
-    printf("\nMensaje recibido: %s\n", buffer);
-    close(clientfd);
+    printf("Coneccion: %s\n", buffer);
 
+    // menu loop
+    char command = 'x';
+	while (command != 's') {
+
+		// imprime menu
+		menu();
+
+		// solicita el comando a ejecutar
+		printf("Comando>> ");
+		do
+			command = getchar();
+		while (isspace(command));
+
+		// elige funcion a realizar
+		switch (tolower(command)) {
+
+			// ingresar registro
+			case 'i':
+                r = send(clientfd, "i", 1, 0);
+                if(r < 0) {
+                    perror("\n-->Error en send(): ");
+                    exit(-1);
+                }
+				create();
+				break;
+
+			// ver registro
+			case 'v':
+                r = send(clientfd, "v", 1, 0);
+                if(r < 0) {
+                    perror("\n-->Error en send(): ");
+                    exit(-1);
+                }
+				list();
+				break;
+
+			// borrar registro
+			case 'o':
+                r = send(clientfd, "o", 1, 0);
+                if(r < 0) {
+                    perror("\n-->Error en send(): ");
+                    exit(-1);
+                }
+				erase();
+				break;
+
+			// buscar registro
+			case 'u':
+                r = send(clientfd, "u", 1, 0);
+                if(r < 0) {
+                    perror("\n-->Error en send(): ");
+                    exit(-1);
+                }
+				search();
+				break;
+
+			// salir
+			case 's':
+				printf("\nGracias y hasta la vista!!!\n");
+
+                // apaga el servidor
+                r = send(clientfd, "c", 1, 0);
+                if(r < 0) {
+                    perror("\n-->Error en send(): ");
+                    exit(-1);
+                }
+
+                // cierra el cliente
+                close(clientfd);
+				break;
+
+			// comando desconocido
+			default:
+				printf("Comando desconocido!\n");
+				break;
+		}
+	}
 }
--- practicas/segunda/p2-dogServer.c
+++ practicas/segunda/p2-dogServer.c
@@ -10,50 +10,97 @@
 
 #define PORT 3535
 #define BACKLOG 2
+#define FILE_NAME "dataDogs.dat"
+#define LOG_NAME "serverDogs.log"
 
-int main(){
+void create() {
+    printf("create\n");
+}
 
+void list() {
+    printf("list\n");
+}
+
+void erase() {
+    printf("erase\n");
+}
+
+void search() {
+    printf("search\n");
+}
+
+int main() {
     int serverfd, clientfd, r, opt = 1;
     struct sockaddr_in server, client;
     socklen_t tamano;
+    char buffer[1];
 
+    // crea el socket
     serverfd = socket(AF_INET, SOCK_STREAM, 0);
     if(serverfd < 0){
         perror("\n-->Error en socket():");
         exit(-1);
     }
 
+    // configura el socket
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = INADDR_ANY;
     bzero(server.sin_zero, 8);
-
     setsockopt(serverfd,SOL_SOCKET,SO_REUSEADDR,(const char *)&opt,sizeof(int));
 
     r = bind(serverfd, (struct sockaddr *)&server, sizeof(struct sockaddr));
-    if(r < 0){
+    if(r < 0) {
         perror("\n-->Error en bind(): ");
         exit(-1);
     }
 
     r = listen(serverfd, BACKLOG);
-    if(r < 0){
+    if(r < 0) {
         perror("\n-->Error en Listen(): ");
         exit(-1);
     }
 
     clientfd = accept(serverfd, (struct sockaddr *)&client, &tamano);
-    if(clientfd < 0)
-    {
+    if(clientfd < 0) {
         perror("\n-->Error en accept: ");
         exit(-1);
     }
 
-    r = send(clientfd, "hola mundo", 10, 0);
-    if(r < 0){
+    // prueba de coneccion
+    r = send(clientfd, "exitosa", 8, 0);
+    if(r < 0) {
         perror("\n-->Error en send(): ");
         exit(-1);
     }
+
+    // recibe comandos
+    do {
+        r = recv(clientfd, buffer, 1, 0);
+        switch (buffer[0]) {
+
+			// ingresar registro
+			case 'i':
+				create();
+				break;
+
+			// ver registro
+			case 'v':
+				list();
+				break;
+
+			// borrar registro
+			case 'o':
+				erase();
+				break;
+
+			// buscar registro
+			case 'u':
+				search();
+				break;
+        }
+    }
+    while (buffer[0] != 'c');
     close(clientfd);
     close(serverfd);
 }
